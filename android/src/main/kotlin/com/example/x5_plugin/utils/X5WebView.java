9ada45061fabf5c721df29cfa5ab63ba52a6858b
zhengxiangke <zhengxiangke@zhilunkeji.com>
2019-11-15 11:54:33 +0800

    webview

--- android/src/main/kotlin/com/example/x5_plugin/utils/X5WebView.java
+++ android/src/main/kotlin/com/example/x5_plugin/utils/X5WebView.java
@@ -62,30 +62,30 @@ public class X5WebView extends WebView {
 		// settings 的设计
 	}
 
-	@Override
-	protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
-		boolean ret = super.drawChild(canvas, child, drawingTime);
-		canvas.save();
-		Paint paint = new Paint();
-		paint.setColor(0x7fff0000);
-		paint.setTextSize(24.f);
-		paint.setAntiAlias(true);
-		if (getX5WebViewExtension() != null) {
-			canvas.drawText(this.getContext().getPackageName() + "-pid:"
-					+ android.os.Process.myPid(), 10, 50, paint);
-			canvas.drawText(
-					"X5  Core:" + QbSdk.getTbsVersion(this.getContext()), 10,
-					100, paint);
-		} else {
-			canvas.drawText(this.getContext().getPackageName() + "-pid:"
-					+ android.os.Process.myPid(), 10, 50, paint);
-			canvas.drawText("Sys Core", 10, 100, paint);
-		}
-		canvas.drawText(Build.MANUFACTURER, 10, 150, paint);
-		canvas.drawText(Build.MODEL, 10, 200, paint);
-		canvas.restore();
-		return ret;
-	}
+//	@Override
+//	protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
+//		boolean ret = super.drawChild(canvas, child, drawingTime);
+//		canvas.save();
+//		Paint paint = new Paint();
+//		paint.setColor(0x7fff0000);
+//		paint.setTextSize(24.f);
+//		paint.setAntiAlias(true);
+//		if (getX5WebViewExtension() != null) {
+//			canvas.drawText(this.getContext().getPackageName() + "-pid:"
+//					+ android.os.Process.myPid(), 10, 50, paint);
+//			canvas.drawText(
+//					"X5  Core:" + QbSdk.getTbsVersion(this.getContext()), 10,
+//					100, paint);
+//		} else {
+//			canvas.drawText(this.getContext().getPackageName() + "-pid:"
+//					+ android.os.Process.myPid(), 10, 50, paint);
+//			canvas.drawText("Sys Core", 10, 100, paint);
+//		}
+//		canvas.drawText(Build.MANUFACTURER, 10, 150, paint);
+//		canvas.drawText(Build.MODEL, 10, 200, paint);
+//		canvas.restore();
+//		return ret;
+//	}
 
 	public X5WebView(Context arg0) {
 		super(arg0);
